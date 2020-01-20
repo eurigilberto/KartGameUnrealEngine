f6a8fb55bf19fa9d0b30f2f3efa369e0b2fffa58
unknown <euri.herasme@gmail.com>
2020-01-20 04:34:17 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# KartClone
+
+Developed with Unreal Engine 4
---
+++ Source/KartClone/KartCloneGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KartCloneGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KARTCLONE_API AKartCloneGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
